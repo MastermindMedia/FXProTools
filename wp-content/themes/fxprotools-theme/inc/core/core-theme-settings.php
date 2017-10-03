e213ffe28640f39447de7e8c6d26c061a3470ce8
ben <benedicksahagun@icloud.com>
2017-10-03 10:44:01 +0800

    add forgot password page form url

--- wp-content/themes/fxprotools-theme/inc/core/core-theme-settings.php
+++ wp-content/themes/fxprotools-theme/inc/core/core-theme-settings.php
@@ -80,7 +80,7 @@ if(!class_exists('ThemeSettings')){
 		}
 
 
-		function customer_login_redirect( $redirect_to, $request, $user ){
+		public function customer_login_redirect( $redirect_to, $request = '', $user = '' ){
 		    return home_url('dashboard');
 		}
 
