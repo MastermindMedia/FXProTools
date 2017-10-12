3f4358b9b19138ce5966dbe9bdb046829181e8d0
Austin Nicomedez <austin.nicomedez@gmail.com>
2017-10-13 00:12:13 +0800

    update - initial dynamic menu nav per page template
    working on a custom nav walker.

--- wp-content/themes/fxprotools-theme/inc/templates/nav-wallet.php
+++ wp-content/themes/fxprotools-theme/inc/templates/nav-wallet.php
@@ -3,6 +3,41 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
+
+				<?php
+					// Get group menus fields
+					$menus = rwmb_meta( 'group_pto_menus' );
+					$header_menu = $menus['select_pto_display_header_menu'];
+					$secondary_hmenu = $menus['taxonomy_pto_secondary_header_menu'];
+				?>
+
+				<?php if($header_menu == 'yes' || $header_menu == 'default') : ?>
+
+				<div class="hide">
+					<?php
+						$params = array(
+							'theme_location'  => '',
+							'menu'            => $secondary_hmenu,
+							'container'       => false,
+							'container_class' => '',
+							'container_id'    => '',
+							'menu_class'      => 'fx-nav-options',
+							'menu_id'         => $secondary_hmenu,
+							'echo'            => true,
+							'fallback_cb'     => 'wp_page_menu',
+							'before'          => '',
+							'after'           => '',
+							'link_before'     => '',
+							'link_after'      => '',
+							'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+							'depth'           => 0,
+							'walker'          => ''
+						);
+						wp_nav_menu( $params );
+					?>
+				</div>
+
+
 				<ul class="fx-nav-options">
 					<li class="dashboard">
 						<a href="<?php bloginfo('url'); ?>/wallet/" class="icon icon-wallet">Wallet</a>
@@ -35,6 +70,9 @@
 						</a>
 					</li>
 				</ul>
+				<?php else : ?>
+					<p>No menu selected.</p>
+				<?php endif; ?>
 			</div>
 		</div>
 	</div>
