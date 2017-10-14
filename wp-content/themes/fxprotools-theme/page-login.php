affd62749373c3a98e11f13d02b942b5976084b0
Morris Chavez <morris.chavez@hiveempire.com>
2017-10-15 00:35:07 +0800

    #66 add jiggle effect when credentials submitted are invalid

--- wp-content/themes/fxprotools-theme/page-login.php
+++ wp-content/themes/fxprotools-theme/page-login.php
@@ -25,8 +25,10 @@
 			<p class="small">You can sign into your account by using the username or email and password used during registration process</p>
 			<?php
 			$username = '';
+			$error_class = '';
 			if ( isset( $_GET['username'] ) ) {
 				$username = sanitize_text_field( $_GET['username'] );
+				$error_class = 'has-error';
 			}
 			if ( isset( $_GET['login'] ) && $_GET['login'] == 'failed' ) : ?>
                 <p class="small error">ERROR: The password you entered for the username <strong><?= $username; ?></strong> is incorrect.
@@ -34,7 +36,7 @@
                 </p>
                 <br/>
 			<?php endif; ?>
-            <form action="<?php echo site_url('wp-login.php?action=login', 'login_post') ?>" method="post">
+            <form action="<?php echo site_url('wp-login.php?action=login', 'login_post') ?>" method="post" class="<?= $error_class; ?>">
 				<div class="form-group">
 					<input type="text" class="form-control no-border-radius" id="email" name="log" value="<?= $username ;?>" required placeholder="username or email">
 				</div>
