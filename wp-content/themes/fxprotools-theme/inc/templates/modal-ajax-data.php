d02e2f12f32908782b9ce03c8a73602f57118a02
apyc <allan.paul.casilum@gmail.com>
2017-10-17 20:42:48 +0800

    update modal ajax data

--- wp-content/themes/fxprotools-theme/inc/templates/modal-ajax-data.php
+++ wp-content/themes/fxprotools-theme/inc/templates/modal-ajax-data.php
@@ -3,8 +3,8 @@
 			<p class="no-webinars"><?php echo $webinars['msg'];?></p>
 	<?php }else{ ?>
 		<?php foreach($webinars as $k => $v){ ?>
-			<p id="<?php echo $v['parse']['key'];?>"><input name="webinars[]" type="checkbox" class="form-check-input" value="<?php echo $v['parse']['key'];?>">
-			<?php echo $v['parse']['startTime'];?></p>
+			<p class="<?php echo $v['parse']['key'];?>-container"><input name="webinars[]" type="checkbox" class="form-check-input" value="<?php echo $v['parse']['key'];?>">
+			<?php echo $v['parse']['startTime'];?><span class="<?php echo $v['parse']['key'];?>-info"></span></p>
 		<?php } ?>
 		<button class="btn btn-primary webinar-register-now">Register Now</button>
 	<?php } ?>
