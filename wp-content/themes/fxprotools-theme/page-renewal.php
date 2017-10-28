19788b6b9522c0289628645663b40a5620d6bdc3
Morris Chavez <morris.chavez@hiveempire.com>
2017-10-28 23:56:09 +0800

    #71 `/renewal` page

--- wp-content/themes/fxprotools-theme/page-renewal.php
+++ wp-content/themes/fxprotools-theme/page-renewal.php
@@ -1,3 +1,68 @@
 <?php get_header(); ?>
+<div class="fx-access-denied">
+    <div class="fx-access-denied-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="fx-header-title">
+                        <h1>It Looks Like You Do Not Have Access</h1>
+                        <p>It looks like in order for you to see this page / content you will need to activate or upgrade your account.<br/>
+                            You just need to pick a package & we can unlock the page for you instantly!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row m-t-md m-b-lg">
+				<?php
+				$membership_product_id = Woocommerce_Settings::MEMBERSHIP_PRODUCTS_ID;
+				$membership_product = wc_get_product( $membership_product_id );
+				$product_ids = $membership_product->get_children();
 
+				foreach ( $product_ids as $product_name => $product_id ) :
+					$product = wc_get_product( $product_id );
+
+					if ( isset( $product ) ) {
+						$product_name = $product->get_name();
+						$product_price = WC_Subscriptions_Product::get_sign_up_fee( $product );
+						$product_price = $product_price == 0 ? $product->get_regular_price() : $product_price;
+						$attributes = $product->get_attribute( 'inclusions' );
+						$attribute_list = [];
+						if ( ! empty( $attributes ) ) {
+							$attribute_list = array_map( 'trim', explode( "|", $attributes ) );
+						}
+					}
+					?>
+                    <div class="col-md-4">
+                        <div class="panel fx-package-item active">
+                            <span class="sash">UPGRADE</span>
+                            <div class="panel-body">
+                                <div class="heading">
+                                    <h3 class="text-normal">Forex & Binary Options</h3>
+                                    <h1 class="m-t-none"><?php echo $product_name; ?> Package</h1>
+                                </div>
+                                <div class="text-center">
+                                    <h2 class="m-b-md"><?php echo wc_price( $product_price ); ?> / month</h2>
+
+									<?php if ( ! empty( $attribute_list ) ) : ?>
+                                        <ul class="list-checklist fa-ul m-t-md m-b-md text-left">
+											<?php foreach ( $attribute_list as $attribute ) : ?>
+                                                <li><i class="fa fa-check fa-li text-green"></i>
+                                                    <p> <?= $attribute; ?> </p></li>
+											<?php endforeach; ?>
+                                        </ul>
+									<?php endif; ?>
+
+                                    <a href="<?php echo get_the_permalink( $product->get_id() ); ?>" class="btn btn-danger block btn-lg m-b-md btn-lg-w-text">
+                                        Get Instant Access Now!
+                                        <span>Training + Forex &amp; Binary Auto Trader</span>
+                                    </a>
+                                    <p class="text-bold">Downgrade / or Cancel At Anytime!</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+				<?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+</div>
 <?php get_footer(); ?>
