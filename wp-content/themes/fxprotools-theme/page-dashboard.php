ecc02aaebc662160738990aafa16aff5e60b394d
Morcen Chavez <hello@morcen.com>
2017-11-04 15:50:19 +0800

    #20 fix gauge svg

--- wp-content/themes/fxprotools-theme/page-dashboard.php
+++ wp-content/themes/fxprotools-theme/page-dashboard.php
@@ -68,15 +68,19 @@ $angle = GAUGE_BASE + ( $average * $accomplished );
 										</div>
 										<div class="group-counter">
                                             <svg id="meter" viewBox="0 0 217.36 118.8">
-                                                <circle r="75" cx="50%" cy="100%" stroke="#DDD"
+                                                <circle r="75" cx="50%" cy="95%" stroke="#DDD"
                                                         stroke-width="60" fill="none"></circle>
-                                                <circle r="75" cx="50%" cy="100%" stroke="#008774"
+                                                <circle r="75" cx="50%" cy="95%" stroke="#008774"
                                                         stroke-width="60" fill="none" stroke-dasharray="<?= $angle; ?>, 943"></circle>
+                                                <?php if ($accomplished < 7): ?>
+                                                <circle r="75" cx="50%" cy="94%" stroke="#DDD"
+                                                        stroke-width="60" fill="none" stroke-dasharray="<?php echo $accomplished > 0 ? 200:240; ?>, 943"></circle>
+                                                <?php endif; ?>
                                                 <g class="danger-dial-tuner" transform="rotate(126 108.93 111.42)" style="transform: rotate(<?= ceil( $accomplished / 7 * 180 ); ?>deg);transform-origin: 108.93px 111.42px 0px;">
                                                     <path class="danger-dial-tuner__needle"
                                                           d="M109.82,104.28l-1.6-.13h0c-18-1.25-55.68,7.26-55.68,7.26s37.66,8.51,55.69,7.26h0.16a7.3,7.3,0,0,0,1.45-.15c5.22-.4,7.16-7.12,7.16-7.12S115,104.67,109.82,104.28Z"
                                                           transform="translate(0 0.01)"></path>
-                                                    <circle class="danger-dial-tuner__knob" cx="108.93" cy="111.42" r="4.1"></circle>
+                                                    <circle class="danger-dial-tuner__knob" cx="108.93" cy="111.42" r="4.1" style="fill: #fff;"></circle>
                                                 </g>
                                             </svg>
                                             <span class="number"><?= $accomplished; ?></span> of <span class="number">7</span>
