e8e345d8d6467f2e0fb6e25b30786d7076bf724f
Steven G <stevengerritse@gmail.com>
2017-11-20 15:22:42 -0500

    Matrix / Holding Tank Adjustment

--- wp-content/themes/fxprotools-theme/page-matrix-tree.php
+++ wp-content/themes/fxprotools-theme/page-matrix-tree.php
@@ -2,6 +2,8 @@
 
 	<?php get_template_part('inc/templates/nav-team'); ?>
 
+
+
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
@@ -10,29 +12,24 @@
 					<p>Check Below For Your Full Matrix Tree</p>
 				</div>
 			</div>
-		</div>
 		<div class="row">
 			<div class="col-md-12">
 				<?php echo do_shortcode('[afl_eps_matrix_genealogy_tree]'); ?>
 			</div>
 		</div>
-	</div>
-
-
-
-	<div class="row">
-		<div class="col-md-12">
-			<div class="fx-header-title">
-				<h1>Your Holding Tank</h1>
-				<p>Check Below For Distributors Waiting for Placement</p>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="fx-header-title">
+					<h1>Your Holding Tank</h1>
+					<p>Check Below For Distributors Waiting for Placement</p>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<?php echo do_shortcode('[afl_eps_unilevel_holding_tank]'); ?>
 			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="col-md-12">
-			<?php echo do_shortcode('[afl_eps_unilevel_holding_tank]'); ?>
-		</div>
-	</div>
-</div>
 
 <?php get_footer(); ?>
