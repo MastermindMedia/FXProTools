a2e0f135255b3cc6618e4db963e5d459df63c0b0
elvin <jonelvinnaorbe@gmail.com>
2017-11-20 14:47:03 +0800

    FIx basic training tabs

--- wp-content/themes/fxprotools-theme/page-basic-training.php
+++ wp-content/themes/fxprotools-theme/page-basic-training.php
@@ -21,21 +21,32 @@ $child_categories = get_course_category_children($category->term_id);
 				<div class="col-md-12">
 					<div role="tabpanel">
 						<ul class="nav nav-tabs fx-tabs" role="tablist">
-							<?php $term_counter = 0; ?>
-							<?php foreach($child_categories as $key => $category): ?>
-								<?php 
-									$term_status = get_term_meta( $category->term_id, 'category_status', true );
-									if($term_status != "draft"): 
-								?>
-										<li role="presentation" class="<?php echo $term_counter == 0 ? 'active' : 'false';?>">
-											<a href="#category-<?php echo $key + 1;?>" aria-controls="category-<?php echo $key + 1;?>" role="tab" data-toggle="tab">
-											<?php echo $category->name;?></a>
-										</li>
-								<?php 
-									$term_counter++;
-									endif; 
-								?>
-							<?php endforeach;?>
+							<?php 
+							$term_counter = 0;
+							$active_categories = 0;
+							foreach($child_categories as $key => $category){
+								$term_status = get_term_meta( $category->term_id, 'category_status', true );
+								if($term_status != "draft"){
+									$active_categories++;
+								}
+							}
+							if($active_categories < 1):
+							?>
+								<?php foreach($child_categories as $key => $category): ?>
+									<?php 
+										$term_status = get_term_meta( $category->term_id, 'category_status', true );
+										if($term_status != "draft"): 
+									?>
+											<li role="presentation" class="<?php echo $term_counter == 0 ? 'active' : 'false';?>">
+												<a href="#category-<?php echo $key + 1;?>" aria-controls="category-<?php echo $key + 1;?>" role="tab" data-toggle="tab">
+												<?php echo $category->name;?></a>
+											</li>
+									<?php 
+										$term_counter++;
+										endif; 
+									?>
+								<?php endforeach;?>
+							<?php endif; ?>
 						</ul>
 						<div class="tab-content">
 							<?php $term_counter = 0; ?>
