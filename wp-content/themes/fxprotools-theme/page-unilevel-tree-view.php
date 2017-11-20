1b3fd578768a258122c437f322bd40232ea4123f
Steven G <stevengerritse@gmail.com>
2017-11-20 15:26:23 -0500

    Title on Unilevel Tree

--- wp-content/themes/fxprotools-theme/page-unilevel-tree-view.php
+++ wp-content/themes/fxprotools-theme/page-unilevel-tree-view.php
@@ -5,6 +5,13 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
+				<div class="fx-header-title">
+					<h1>Your Matrix Tree</h1>
+					<p>Check Below For Your Full Matrix Tree</p>
+				</div>
+			</div>
+		<div class="row">
+			<div class="col-md-12">
 				<?php echo do_shortcode('[afl_eps_unilevel_genealogy_tree]'); ?>
 			</div>
 		</div>
