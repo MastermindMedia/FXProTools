030139021f71814307abba2369ddcb28ca7b89cc
Steven G <stevengerritse@gmail.com>
2017-11-30 15:29:02 -0500

    Changed "Free Shirt" to "Discounted Shirt"

--- wp-content/themes/fxprotools-theme/page-dashboard.php
+++ wp-content/themes/fxprotools-theme/page-dashboard.php
@@ -1,7 +1,7 @@
 <?php
 $checklist = get_user_checklist();
 
-// Get Your Free Shirt
+// Get Your CPS Shirt
 if ( ! $checklist['got_shirt'] ) {
 	if ( Woocommerce_Settings::has_claimed_shirt() ) {
 		pass_onboarding_checklist( 'got_shirt' );
@@ -67,8 +67,8 @@ $dashboard_checklist = [
 		'subtext' => 'Don\'t miss out weekly Q&A webinars to answer all your questions, click the "Reserve A Seat" button.'
 	],
 	'got_shirt'         => [
-		'title'   => 'Get your free shirt',
-		'subtext' => 'Receive one FREE T-shirt from our store by clicking on "Get Free Shirt Button" or by <a href="/product/free-shirt">clicking here</a>.'
+		'title'   => 'Get your CPS Shirt',
+		'subtext' => 'Get A CPS T-shirt from our store 75% OFF by clicking on "Get CPS Shirt Button" or by <a href="/product/copy-profit-success-tshirt/">clicking here</a>.'
 	],
 	'accessed_products' => [
 		'title'   => 'Access your product',
