d317bd5e1a31a61c15693c342e4cbc925f21b98a
John Benedict <jben0324@gmail.com>
2017-12-03 22:24:53 +0800

    Remove extra text in variation add to cart

--- wp-content/themes/fxprotools-theme/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ wp-content/themes/fxprotools-theme/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -43,7 +43,7 @@ global $product;
 
             <button type="submit" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
 
-        <?php } ?>`
+        <?php } ?>
     <input type="hidden" name="add-to-cart" value="<?php echo absint( $product->get_id() ); ?>" />
 	<input type="hidden" name="product_id" value="<?php echo absint( $product->get_id() ); ?>" />
 	<input type="hidden" name="variation_id" class="variation_id" value="0" />
