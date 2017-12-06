211ee37e33110ca2de28a24b90c7eb637dfe30f5
mcaprotools <stevengerritse@gmail.com>
2017-12-06 00:42:22 -0500

    Added Missing Holding Tank on "Holding Tanks" page.

--- wp-content/themes/fxprotools-theme/page-holding-tank.php
+++ wp-content/themes/fxprotools-theme/page-holding-tank.php
@@ -2,11 +2,30 @@
 
 	<?php get_template_part('inc/templates/nav-team'); ?>
 
+
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
 				<div class="fx-header-title">
-					<h1>Your Holding Tank</h1>
+					<h1>Your "Matrix" Holding Tank</h1>
+					<p>Check Below For Distributors Waiting for Placement:</p>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<?php echo do_shortcode('[afl_eps_matrix_holding_tank]'); ?>
+			</div>
+		</div>
+	</div>
+
+
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="fx-header-title">
+					<h1>Your "Uni-Level" Holding Tank</h1>
 					<p>Check Below For Distributors Waiting for Placement:</p>
 				</div>
 			</div>
