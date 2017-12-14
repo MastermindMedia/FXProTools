188e121891349877f31b884f0b2c369634de3258
John Benedict <jben0324@gmail.com>
2017-12-14 23:21:31 +0800

    Update - Added Sponsor info and team info in genealogy tab

--- wp-content/themes/fxprotools-theme/inc/templates/my-account/genealogy.php
+++ wp-content/themes/fxprotools-theme/inc/templates/my-account/genealogy.php
@@ -1,2 +1,4 @@
 <p class="text-bold">Genealogy</p>
+<?php echo do_shortcode('[afl_sponsor_info]'); ?>
+<?php echo do_shortcode('[afl_team_info]'); ?>
 <?php echo do_shortcode('[afl_eps_matrix_genealogy_tree]'); ?>
