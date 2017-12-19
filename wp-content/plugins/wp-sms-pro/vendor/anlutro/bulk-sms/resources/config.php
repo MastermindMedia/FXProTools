e9a3e97551387a211e66a99eb568d6b13226bd0d
Austin Nicomedez <austin.nicomedez@gmail.com>
2017-12-19 23:22:39 +0800

    remove some plugins

--- wp-content/plugins/wp-sms-pro/vendor/anlutro/bulk-sms/resources/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-    'username' => 'REPLACEME',
-    'password' => 'REPLACEME',
-
-    'baseurl' => 'http://bulksms.vsms.net:5567',
-    // 'baseurl' => 'http://bulksms.com',
-    // 'baseurl' => 'http://bulksms.de',
-);
