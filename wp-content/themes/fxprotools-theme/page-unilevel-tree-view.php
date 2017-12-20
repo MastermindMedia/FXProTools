249c4d00ef51eec6b3fbbb716ca6a28c9bdb5aca
John Benedict <jben0324@gmail.com>
2017-12-20 23:19:14 +0800

    Unilevel tree adjustment

--- wp-content/themes/fxprotools-theme/page-unilevel-tree-view.php
+++ wp-content/themes/fxprotools-theme/page-unilevel-tree-view.php
@@ -16,13 +16,16 @@ get_header();
 				</div>
 			</div>
 		</div>
+	</div>
+	<div class="container-fluid">
 		<div class="row">
-			<div class="col-md-12">
+			<div class="col-md-12 epx">
 				<?php echo do_shortcode('[afl_unilevel_holding_tank_genealogy_toggle_placement]'); ?>
 			</div>
 		</div>
+	</div>
 
-<div class="container">
+	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
 				<div class="fx-header-title">
@@ -37,23 +40,23 @@ get_header();
 			</div>
 		</div>
 	</div>
-</div>
 
-<div class="container">
-	<div class="row">
-		<div class="col-md-12">
-			<div class="fx-header-title">
-				<h1>Unilevel Direct Downline</h1>
-				<p>Check Below For Your Unilevel Direct Downline</p>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="fx-header-title">
+					<h1>Unilevel Direct Downline</h1>
+					<p>Check Below For Your Unilevel Direct Downline</p>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<?php echo do_shortcode('[afl_eps_unilevel_downlines]'); ?>
 			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="col-md-12">
-			<?php echo do_shortcode('[afl_eps_unilevel_downlines]'); ?>
-		</div>
-	</div>
-</div>
 
 
 <?php get_footer(); ?>
