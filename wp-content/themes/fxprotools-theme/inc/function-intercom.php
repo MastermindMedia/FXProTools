2a427bfe475b5abe34317fb52ba4309469ea4b7f
Morcen Chavez <hello@morcen.com>
2017-12-29 17:32:11 +0800

    remove intercom

--- wp-content/themes/fxprotools-theme/inc/function-intercom.php
+++ wp-content/themes/fxprotools-theme/inc/function-intercom.php
@@ -48,9 +48,9 @@ class CPS_Intercom {
 	 */
 	public function __construct() {
 		$this->client = new IntercomClient( self::ACCESS_TOKEN, null );
-		add_action( 'user_register', [ $this, 'intercom_add_user' ] );
-		add_action( 'profile_update', [ $this, 'intercom_update_user' ] );
-		add_action( 'delete_user', [ $this, 'intercom_delete_user' ] );
+//		add_action( 'user_register', [ $this, 'intercom_add_user' ] );
+//		add_action( 'profile_update', [ $this, 'intercom_update_user' ] );
+//		add_action( 'delete_user', [ $this, 'intercom_delete_user' ] );
 	}
 
 	/**
@@ -137,12 +137,14 @@ class CPS_Intercom {
 	 */
 	private function create_user( array $data ) {
 		$user = new IntercomUsers( $this->client );
-		try {
-			/** @var IntercomUsers */
-			return $user->create( $data );
-		} catch ( GuzzleException $e ) {
-			error_log( $e->getMessage() );
-		}
+		var_dump($data);
+		exit;
+//		try {
+//			/** @var IntercomUsers */
+//			return $user->create( $data );
+//		} catch ( GuzzleException $e ) {
+//			error_log( $e->getMessage() );
+//		}
 	}
 
 	/**
