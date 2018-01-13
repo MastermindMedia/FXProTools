e0e6bc85044c355c5bc5a9ed4bc44977eb4147f0
tarundhanwani <tarundhanwani@gmail.com>
2018-01-13 13:57:33 +0800

    updating past webinars

--- wp-content/themes/fxprotools-theme/inc/templates/coaching/past-webinar.php
+++ wp-content/themes/fxprotools-theme/inc/templates/coaching/past-webinar.php
@@ -1,7 +1,26 @@
 <div class="container">
 	<div class="webinar-list-past">
 		<div class="table-responsive">
-		  <div id="ajax-coach-history-webinars"></div>
+		  <div id="past-webinars">
+			<table>
+			  <thead>
+			    <tr>
+			      <th scope="col">Date</th>
+			      <th scope="col">Time</th>
+			      <th scope="col">Title</th>
+			      <th scope="col">Join Link</th>
+			    </tr>
+			  </thead>
+			  <tbody>
+			    <tr>
+			      <td data-label="Account">2018-02-08</td>
+			      <td data-label="Due Date">17:00</td>
+			      <td data-label="Amount">Free Weekly Q&A</td>
+			      <td data-label="Period">Watch Replay</td>
+			    </tr>
+			  </tbody>
+			</table>
+		  </div>
 		</div>	
 	</div>
 </div>
