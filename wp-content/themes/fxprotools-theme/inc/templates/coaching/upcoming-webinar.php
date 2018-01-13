ddcd1cca3f94587e469105f62e05c242a29675f4
tarundhanwani <tarundhanwani@gmail.com>
2018-01-13 13:21:08 +0800

    adding additional changes

--- wp-content/themes/fxprotools-theme/inc/templates/coaching/upcoming-webinar.php
+++ wp-content/themes/fxprotools-theme/inc/templates/coaching/upcoming-webinar.php
@@ -6,36 +6,36 @@
 			  <caption>Statement Summary</caption>
 			  <thead>
 			    <tr>
-			      <th scope="col">Account</th>
-			      <th scope="col">Due Date</th>
-			      <th scope="col">Amount</th>
-			      <th scope="col">Period</th>
+			      <th scope="col">Date</th>
+			      <th scope="col">Time</th>
+			      <th scope="col">Title</th>
+			      <th scope="col">Join Link</th>
 			    </tr>
 			  </thead>
 			  <tbody>
 			    <tr>
-			      <td data-label="Account">Visa - 3412</td>
-			      <td data-label="Due Date">04/01/2016</td>
-			      <td data-label="Amount">$1,190</td>
-			      <td data-label="Period">03/01/2016 - 03/31/2016</td>
+			      <td data-label="Account">2018-02-08</td>
+			      <td data-label="Due Date">17:00</td>
+			      <td data-label="Amount">Free Weekly Q&A</td>
+			      <td data-label="Period">Join Meeting</td>
 			    </tr>
 			    <tr>
-			      <td scope="row" data-label="Account">Visa - 6076</td>
-			      <td data-label="Due Date">03/01/2016</td>
-			      <td data-label="Amount">$2,443</td>
-			      <td data-label="Period">02/01/2016 - 02/29/2016</td>
+			      <td data-label="Account">2018-02-08</td>
+			      <td data-label="Due Date">17:00</td>
+			      <td data-label="Amount">Free Weekly Q&A</td>
+			      <td data-label="Period">Join Meeting</td>
 			    </tr>
 			    <tr>
-			      <td scope="row" data-label="Account">Corporate AMEX</td>
-			      <td data-label="Due Date">03/01/2016</td>
-			      <td data-label="Amount">$1,181</td>
-			      <td data-label="Period">02/01/2016 - 02/29/2016</td>
+			      <td data-label="Account">2018-02-08</td>
+			      <td data-label="Due Date">17:00</td>
+			      <td data-label="Amount">Free Weekly Q&A</td>
+			      <td data-label="Period">Join Meeting</td>
 			    </tr>
 			    <tr>
-			      <td scope="row" data-label="Acount">Visa - 3412</td>
-			      <td data-label="Due Date">02/01/2016</td>
-			      <td data-label="Amount">$842</td>
-			      <td data-label="Period">01/01/2016 - 01/31/2016</td>
+			      <td data-label="Account">2018-02-08</td>
+			      <td data-label="Due Date">17:00</td>
+			      <td data-label="Amount">Free Weekly Q&A</td>
+			      <td data-label="Period">Join Meeting</td>
 			    </tr>
 			  </tbody>
 			</table>
