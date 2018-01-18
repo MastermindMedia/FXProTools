4cc7a6451910921c40aecd226231e84fd656aafe
tarundhanwani <tarundhanwani@gmail.com>
2018-01-18 10:26:07 +0800

    adding video code

--- wp-content/themes/fxprotools-theme/single-fx_webinar.php
+++ wp-content/themes/fxprotools-theme/single-fx_webinar.php
@@ -50,6 +50,38 @@ get_header();
                            <div class="webinar-button">
                               <a href="http://google.com" class="btn btn-danger btn-lg btn-lg-w-text scroll-to">Registration Link for Webinar</a>
                            </div>
+
+                        <?php 
+                           else :
+                        ?>
+                           <div class="webinar-summary">
+                              <h4 style="margin-top:0;">Webinar Title:</h4>
+                              <h1 style="margin-bottom: 30px; margin-top: 10px;">Free Weekly Q&amp;A</h1>
+
+                              <h4>Description</h4>
+                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                           </div>
+                           <div class="webinar-details">
+                              <div class="webinar-details-block">
+                                 <h4>Webinar Date:</h4>
+                                 <h5>2018-02-08</h5>
+                              </div>
+                              <div class="webinar-details-block">
+                                 <h4>Start Time:</h4>
+                                 <h5>16:00</h5>
+                              </div>
+                              <div class="webinar-details-block">
+                                 <h4>End Time:</h4>
+                                 <h5>17:00</h5>
+                              </div>
+                           </div>
+                           <div class="webinar-button">
+                              <a href="http://google.com" class="btn btn-danger btn-lg btn-lg-w-text scroll-to">Watch the Webinar Replay Below</a>
+                           </div>
+                           <div>
+                              <img src="/wp-content/themes/fxprotools-theme/assets/img/video-placeholder.png" />
+                           </div>
+
                         <?php 
                            endif;
                         ?>
