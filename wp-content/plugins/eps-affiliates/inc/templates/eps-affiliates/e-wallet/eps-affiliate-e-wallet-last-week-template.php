1a92159b4173588219f412241cfe1d17cd7a712b
Steven G <stevengerritse@gmail.com>
2018-02-03 00:53:41 -0500

    Remove EPS Plugin.

--- wp-content/plugins/eps-affiliates/inc/templates/eps-affiliates/e-wallet/eps-affiliate-e-wallet-last-week-template.php
+++
@@ -1,6 +0,0 @@
-<div class="panel panel-default text-center m-r-n-xs">
-	<div class="panel-body">
-		<span><?= __('Last Week'); ?></span>
-		<h3 class="m-t-xs">$<?= apply_filters('afl_ewallet_last_week_earnings',get_uid()) ?></h3>
-	</div>
-</div>
