b6cae7860bf6611922626165b06a98dc5b87b8c8
DESKTOP-6PJJSNH\John Benedict <jben0324@gmail.com>
2018-08-29 22:37:34 +0800

    move google font script too footer for page optimization

--- wp-content/themes/fxprotools-theme/header.php
+++ wp-content/themes/fxprotools-theme/header.php
@@ -17,21 +17,6 @@
 		gtag('config', 'UA-110702266-1');
 		</script>
 	<?php endif; ?>
-	
-	<script type="text/javascript">
-		WebFontConfig = {
-		google: { families: [ 'Lato:300,400,700' ] }
-		};
-		(function() {
-		var wf = document.createElement('script');
-		wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-		'://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js';
-		wf.type = 'text/javascript';
-		wf.async = 'true';
-		var s = document.getElementsByTagName('script')[0];
-		s.parentNode.insertBefore(wf, s);
-		})();
-	</script>
 
 	<?php wp_head(); ?>
 </head>
